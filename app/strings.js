09c435c86f65225bf05ace16683ac2886ec06510
Rachel Jenkins <jenkinsr07@gmail.com>
2016-02-16 12:19:18 -0800

    [feat] progress on strings

--- app/strings.js
+++ app/strings.js
@@ -2,12 +2,33 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.stringsAnswers = {
   reduceString: function(str, amount) {
-
+  var count = {};
+  var res = '';
+  var donezo = [];
+  for(var i = 0; i < str.length; i++) {
+    count[str[i]] = count[str[i]] + 1 || 1;
+  }
+  
+  for(var j = 0; j < str.length; j ++) {
+      var counter = 0;
+      if(count[str[j]] === 1) {
+        res += str[j];  
+      } else {
+        while(counter < amount && !_.contains(donezo, str[j])) {
+            res += str[j];
+            counter++;
+        }
+        donezo.push(str[j]);
+      }
+  }
+  return res;
   },
+
   wordWrap: function(str, cols) {
 
   },
-  reverseString: function(str) {
 
+  reverseString: function(str) {
+    return str.split('').reverse().join('');
   }
 };
