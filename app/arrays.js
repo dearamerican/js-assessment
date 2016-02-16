eeedc27568d7931f3246c3e665e68e72c3588b1b
Rachel Jenkins <jenkinsr07@gmail.com>
2016-02-15 21:51:37 -0800

    [feat] arrays complete

--- app/arrays.js
+++ app/arrays.js
@@ -20,19 +20,24 @@ exports.arraysAnswers = {
   },
 
   remove : function(arr, item) {
-    for(var i = 0; i < arr.length; i++) {
-      if(arr[i] === item){
-        arr.splice(i, 1);
+    arrCopy = arr.slice();
+    for(var i = 0; i < arrCopy.length; i++) {
+      if(arrCopy[i] === item){
+        arrCopy.splice(i, 1);
       } 
     }
-    return arr;
+    return arrCopy;
   },
 
   removeWithoutCopy : function(arr, item) {
-    for(var i = 0; i < arr.length; i++) {
-      if(arr[i] === item){
+    var length;
+
+    for (var i = 0, length = arr.length; i < length; i++) {
+      if (arr[i] === item) {
         arr.splice(i, 1);
-      } 
+        i = i - 1;
+        length = length - 1;
+      }
     }
     return arr;
   },
@@ -63,23 +68,49 @@ exports.arraysAnswers = {
   },
 
   insert : function(arr, item, index) {
-    arr.splice(i, 0, item);
+    arr.splice(index, 0, item);
     return arr;
   },
 
   count : function(arr, item) {
-
+    var total = 0;
+    for(var i = 0; i < arr.length; i ++) {
+      if(arr[i] === item) {
+        total += 1;
+      }
+    }
+    return total;
   },
 
   duplicates : function(arr) {
-
+    var countObj = {};
+    var dups = [];
+    for(var i = 0; i < arr.length; i++) {
+      countObj[arr[i]] = countObj[arr[i]] +1 || 1;
+    }
+    for(var key in countObj) {
+      if (countObj[key] > 1) {
+        dups.push(Number(key));
+      }
+    }
+    return dups;
   },
 
   square : function(arr) {
-
+    var sqArr = [];
+    for(var i = 0; i < arr.length; i++) {
+      sqArr.push(arr[i] * arr[i]);
+    }
+    return sqArr;
   },
 
   findAllOccurrences : function(arr, target) {
-
+    var occurences = [];
+    for(var i = 0; i < arr.length; i++) {
+      if (arr[i] === target) {
+        occurences.push(i);
+      }
+    }
+    return occurences;
   }
 };
