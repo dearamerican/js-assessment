e8ea70a14e70a9a90ce83b6d36178ee8ce96369d
Rachel Jenkins <jenkinsr07@gmail.com>
2016-02-18 13:11:42 -0800

    [feat] progress on functions

--- app/functions.js
+++ app/functions.js
@@ -2,19 +2,32 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.functionsAnswers = {
   argsAsArray : function(fn, arr) {
-
+    return fn.apply(this, arr);
   },
 
   speak : function(fn, obj) {
-
+    return fn.bind(obj)();
   },
 
   functionFunction : function(str) {
-
+    var stringCombiner = function(otherStr) {
+      return str + ', ' + otherStr;
+    };
+    return stringCombiner;
   },
 
   makeClosures : function(arr, fn) {
+    var funcArr = [];
 
+    var genFunc = function(num) {
+      return function() {
+        return fn(num);
+      };
+    };
+    for(var i = 0; i < arr.length; i++) {
+      funcArr.push(genFunc(arr[i]));
+    }
+    return funcArr;
   },
 
   partial : function(fn, str1, str2) {
@@ -26,7 +39,7 @@ exports.functionsAnswers = {
   },
 
   callIt : function(fn) {
-
+    return fn();
   },
 
   partialUsingArguments : function(fn) {
