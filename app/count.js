474e4d21d2ea71b6791d26fd34539d9a914d5085
Rachel Jenkins <jenkinsr07@gmail.com>
2016-02-19 14:30:47 -0800

    [feat] count complete

--- app/count.js
+++ app/count.js
@@ -2,6 +2,21 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.countAnswers =  {
   count : function (start, end) {
+    var timeout;
+    function countIt () {
+      console.log(start++);
 
+      if(start <= end) {
+        timeout = setTimeout(countIt, 100);
+      }
+    }
+
+    countIt();
+
+    return {
+      cancel : function () {
+        timeout && clearTimeout(timeout);
+      }
+    };
   }
 };
