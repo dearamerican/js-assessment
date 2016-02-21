ade8e7ef5c13868a1740a9b76f29d1db5f8a6218
Rachel Jenkins <jenkinsr07@gmail.com>
2016-02-20 23:24:44 -0800

    [feat] recursion begun, fibonacci complete

--- app/recursion.js
+++ app/recursion.js
@@ -1,16 +1,22 @@
 exports = (typeof window === 'undefined') ? global : window;
 
 exports.recursionAnswers = {
-  listFiles: function(data, dirName) {
 
   },
 
   permute: function(arr) {
 
+    var recurse = function() {
+
+    };
+
   },
 
   fibonacci: function(n) {
-
+    if(n <= 2) {
+      return 1;
+    } 
+    return exports.recursionAnswers.fibonacci(n-1) + exports.recursionAnswers.fibonacci(n-2);
   },
 
   validParentheses: function(n) {
