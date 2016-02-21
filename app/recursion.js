417b7bf4f3f3c9bf5a4889845d87f53b0bbaed4b
Rachel Jenkins <jenkinsr07@gmail.com>
2016-02-21 12:53:28 -0800

    [feat] recursion complete (file data traversal)

--- app/recursion.js
+++ app/recursion.js
@@ -1,7 +1,50 @@
 exports = (typeof window === 'undefined') ? global : window;
 
 exports.recursionAnswers = {
-
+  listFiles: function(data, dirName) {
+    var res = [];
+    var subFile;
+    var recurse = function(f) {
+      if(dirName) {
+        for(var key in f) {   
+          if(f[key] === dirName || subFile) {
+            for(var i = 0; i < f['files'].length; i++) {
+              if(typeof f['files'][i] === 'string') {
+                res.push(f['files'][i]);
+              } 
+              if(f['files'][i] && typeof f['files'][i] === 'object') {
+                subFile = true;
+                recurse(f['files'][i]);
+              }
+            }
+            subFile = false;
+            return;
+          } 
+          else if (key === 'dir' && f[key] !== dirName) {
+            for(var g = 0; g < f['files'].length; g++) {
+              if(f['files'][g] && typeof f['files'][g] === 'object') {
+                recurse(f['files'][g]);
+              }
+            }
+          }
+        }
+      } else {
+        for(var key in f) {   
+          if (key === 'files') {
+            for(var j = 0; j < f[key].length; j++) {
+              if(typeof f[key][j] === 'string') {
+                res.push(f[key][j]);
+              }
+              if(f[key][j] && typeof f[key][j] === 'object') {
+                recurse(f[key][j]);
+              }
+            }
+          }
+        }
+      }
+    };
+    recurse(data);
+    return res;
   },
 
   permute: function(arr) {
