a8a237f85a5eef1a77aecc0de06b6e7ec31016f2
Rachel Jenkins <jenkinsr07@gmail.com>
2016-02-22 12:07:38 -0800

    [feat] async complete

--- app/async.js
+++ app/async.js
@@ -2,10 +2,26 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.asyncAnswers = {
   async : function(value) {
+    return new Promise(function(resolve, reject) {
+      window.setTimeout(
+        function() {
+          resolve(value);
+        }, Math.random()*2000 + 1000);
+    });
 
   },
 
   manipulateRemoteData : function(url) {
-
+    var arr = [];
+    return $.ajax({
+      method: 'GET',
+      url: url
+    }).then(function(resp) {
+      resp.people.forEach(function(person) {
+        arr.push(person.name);
+      });
+      return arr.sort();
+    });
   }
+
 };
